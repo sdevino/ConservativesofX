73f73b42ca8e05e7985baf094e7bac1a65722855
Spencer Devino <spencer.devino@gmail.com>
2026-03-23 11:46:01 -0500

    Update port and add 404 handler

--- server.js
+++ server.js
@@ -208,7 +208,10 @@ app.use((req, res) => {
 });
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
 
 process.on('SIGTERM', async () => {
   await prisma.$disconnect();
